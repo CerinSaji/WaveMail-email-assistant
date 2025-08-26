6e3e8f8906a09ab0920df894dd32c1c8a5d91c5f
CerinSaji <cerins2003@gmail.com>
2025-08-26 18:20:34 +0400

    Basic ReactJS Frontend - redo commit #3

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from agents.notifications_pipeline import get_notifications
 from agents.todo_pipeline import get_todolist
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:5173", "http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/notifications")
 def notifications(n: int = 4):
